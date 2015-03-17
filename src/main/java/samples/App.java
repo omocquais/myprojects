6f22b235bcedc2db5431b74f858fb5806aac3ec2
Olivier MOCQUAIS <olivier.mocquais@gmail.com>
2015-03-17 23:25:16 +0100

    suppression des erreurs

--- src/main/java/samples/App.java
+++ src/main/java/samples/App.java
@@ -9,8 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        /*
         String test=null;
         if (test.isEmpty())
             System.out.println("pb  ");
+        */
     }
 }
