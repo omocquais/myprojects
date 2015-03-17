cf80c0dba7f6b0c3d6381a9cd90090700f0811df
Olivier MOCQUAIS <olivier.mocquais@gmail.com>
2015-03-17 23:09:47 +0100

    maj du code pour ajouter des violations

--- src/main/java/samples/App.java
+++ src/main/java/samples/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String test=null;
+        if (test.isEmpty())
+            System.out.println("pb  ");
     }
 }
